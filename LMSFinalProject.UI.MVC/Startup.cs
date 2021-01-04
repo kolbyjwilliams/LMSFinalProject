af9034fb020c93ebf22a364be0053f6a52c6d489
kolbyjwilliams <kolbyjwilliams@gmail.com>
2021-01-04 14:53:08 -0600

    Identity samples

---
+++ LMSFinalProject.UI.MVC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace LMSFinalProject.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
